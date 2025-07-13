8a6b99c2cfd3a2054fba0ff8706883594d213877
Varshinisiru <varshinisirusolla@gmail.com>
2025-07-13 12:49:00 +0530

    made changes for PORT and url with homepage

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const productRoutes = require('./routes/productRoutes')
 const cors = require('cors');
 const path = require('path')
 const app = express()
-const PORT =4000;
+const PORT =process.env.PORT || 4000;
 dotEnv.config();
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB connected successfully!"))
@@ -23,6 +23,6 @@ app.use('/uploads', express.static('uploads'));
 app.listen(PORT, ()=>{
     console.log(`server started and running at ${PORT}`);
 });
-app.use('/home', (req,res)=>{
+app.use('/', (req,res)=>{
     res.send("<h1> Welcome to FoodNow");
 })
